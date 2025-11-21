f36430842122572eb953335dae77a7377ed3d97b
Paulo Vitor <paulovitorcarneiro11@hotmail.com>
2025-11-21 01:33:24 -0300

    Add obterTodos and deletarPorCodigo endpoints.

--- clientes/src/main/java/br/com/ecommerce/clientes/controller/ClienteController.java
+++ clientes/src/main/java/br/com/ecommerce/clientes/controller/ClienteController.java
@@ -2,9 +2,13 @@ package br.com.ecommerce.clientes.controller;
 
 import br.com.ecommerce.clientes.model.Cliente;
 import br.com.ecommerce.clientes.service.ClienteService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("clientes")
@@ -25,6 +29,28 @@ public class ClienteController {
                 .obterPorCodigo(codigo)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<?> obterTodos() {
+        var clientes = service.obterTodos();
+        if (clientes.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Não há itens para listar.");
+        }
+        return ResponseEntity.ok(clientes);
+    }
+
+    @DeleteMapping("{codigo}")
+    public ResponseEntity<?> deletarPorCodigo(@PathVariable Long codigo) {
+        try {
+            service.deletarPorCodigo(codigo);
+            return ResponseEntity.ok("Cliente deletado com sucesso. ID: " + codigo);
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }
     }
 
 }
--- clientes/src/main/java/br/com/ecommerce/clientes/service/ClienteService.java
+++ clientes/src/main/java/br/com/ecommerce/clientes/service/ClienteService.java
@@ -2,11 +2,16 @@ package br.com.ecommerce.clientes.service;
 
 import br.com.ecommerce.clientes.model.Cliente;
 import br.com.ecommerce.clientes.repository.ClienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClienteService {
@@ -19,5 +24,17 @@ public class ClienteService {
 
     public Optional<Cliente> obterPorCodigo(Long codigo) {
         return repository.findById(codigo);
+    }
+
+    public List<Cliente> obterTodos() {
+        return repository.findAll();
+    }
+
+    public void deletarPorCodigo(Long codigo) {
+        boolean codigoExiste = repository.existsById(codigo);
+        if (!codigoExiste) {
+            throw new EntityNotFoundException("Cliente não encontrado para o código: " + codigo);
+        }
+        repository.deleteById(codigo);
     }
 }
